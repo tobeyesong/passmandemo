eb001604cfeaff1a89cecbae72fe28ca4d88d8c2
Toan Lam <toanlam01@gmail.com>
2026-03-11 17:06:35 -0700

    fix: restore edit modal cancel buttons

--- frontend/src/components/modal/EditNoteModal.js
+++ frontend/src/components/modal/EditNoteModal.js
@@ -168,8 +168,8 @@ const EditNoteModal = () => {
                               <div className='flex justify-end pt-5'>
                                 <button
                                   type='button'
-                                  className='px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
                                   onClick={handleClose}
+                                  className='px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
                                   Cancel
                                 </button>
                                 <button
--- frontend/src/components/modal/EditPasswordModal.js
+++ frontend/src/components/modal/EditPasswordModal.js
@@ -282,8 +282,8 @@ const EditPasswordModal = () => {
                               <div className='flex justify-end'>
                                 <button
                                   type='button'
-                                  className='px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
                                   onClick={handleClose}
+                                  className='px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
                                   Cancel
                                 </button>
                                 <button
